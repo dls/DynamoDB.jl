fdbd3a870ca35babcbb4cebcec870d8743662a6d
David Salamon <dls@lithp.org>
2015-10-25 04:59:24 -0700

    worked the bugs out of the typed json transformer

--- src/dynamo_json.jl
+++ src/dynamo_json.jl
@@ -4,42 +4,37 @@
 # \__ \  __/ |  | | (_| | | |/ /  __/
 # |___/\___|_|  |_|\__,_|_|_/___\___|
 
-const null_attr = {"NULL" : true}
-# TODO -- binary?
-attribute_value(x :: Bool) = Dict{String, Any}("BOOL" => x)
-attribute_value(x :: Real) = Dict{String, Any}("N" => x)
-attribute_value(x :: String) = Dict{String, Any}("S" => x)
-
-attribute_value(x :: Array) =
-    Dict{String, Any}("L" => [attribute_value(e) for e=x])
-attribute_value(x :: Set{Real}) =
-    Dict{String, Any}("NS" => [attribute_value(e) for e=x])
-attribute_value(x :: Set{String}) =
-    Dict{String, Any}("SS" => [attribute_value(e) for e=x])
-# TODO -- n-dimensional arrays
-
-function attribute_value(x :: Dict)
-    dict = Dict{String, Any}()
-    for (k,v)=x
-        dict[k] = null_or_val(v)
-    endn
-    Dict{String, Any}("M" => dict)
-end
-
-null_or_val(x) = x == nothing ? null_attr : attribute_value(x)
-
-function attribute_value(x :: Dict)
-    r = {}
-    for (k,v)=x ; r[k] = null_or_val(v) ; end
-    {"M" => r}
-end
+const null_attr = Dict{AbstractString, Any}("NULL" => true)
 
 # general objects
 function attribute_value(x)
-    r = {}
+    r = Dict{ASCIIString, Any}()
     for e=fieldnames(x) ; r[string(e)] = null_or_val(x.(e)) ; end
-    {"M" => r}
+    Dict{ASCIIString, Any}("M" => r)
 end
+
+# TODO -- binary?
+attribute_value(x :: Bool) = Dict{AbstractString, Any}("BOOL" => x)
+attribute_value(x :: Real) = Dict{AbstractString, Any}("N" => x)
+attribute_value(x :: AbstractString) = Dict{AbstractString, Any}("S" => x)
+
+attribute_value(x :: Array) =
+    Dict{AbstractString, Any}("L" => [attribute_value(e) for e=x])
+attribute_value{T <: Real}(x :: Set{T}) =
+    Dict{AbstractString, Any}("NS" => [attribute_value(e) for e=x])
+attribute_value{T <: AbstractString}(x :: Set{T}) =
+    Dict{AbstractString, Any}("SS" => [attribute_value(e) for e=x])
+# TODO -- n-dimensional arrays
+
+function attribute_value(x :: Dict)
+    dict = Dict{ASCIIString, Any}()
+    for (k,v)=x
+        dict[string(k)] = null_or_val(v)
+    end
+    Dict{ASCIIString, Any}("M" => dict)
+end
+
+null_or_val(x) = x == nothing ? null_attr : attribute_value(x)
 
 
 #      _                     _       _ _
@@ -48,7 +43,8 @@ end
 # | (_| |  __/\__ \  __/ |  | | (_| | | |/ /  __/
 #  \__,_|\___||___/\___|_|  |_|\__,_|_|_/___\___|
 
-function bool_value_to_attribute(val)
+bool_val(val :: Bool) = val
+function bool_val(val :: AbstractString)
     if lowercase(val) == "false"
         return false
     elseif lowercase(val) == "true"
@@ -58,21 +54,32 @@ function bool_value_to_attribute(val)
     end
 end
 
-function value_to_attribute(hash)
+real_val(val :: Real) = val
+real_val(val :: AbstractString) = parse(Float64, val)
+
+function value_from_attributes(hash :: Dict)
     ks = keys(hash)
     if length(ks) != 1
+        error("multiple type keys provided in DynamoDB typed JSON value")
     end
 
-    ty = ks[1]
-    val = hash[ty]]
+    ty = first(ks)
+    val = hash[ty]
 
     if ty == "NULL"     ; return nothing
-    elseif ty == "BOOL" ; return bool_value_to_attribute(lowercase(val))
-    elseif ty == "N"    ; return parse(Float64, val)
+    elseif ty == "BOOL" ; return bool_val(val)
+    elseif ty == "N"    ; return real_val(val)
     elseif ty == "S"    ; return val
-    elseif ty == "L"    ; return [value_to_attribute(e) for e=val]
-    elseif ty == "NS"   ; return Set([parse(Float64, e) for e=val])
-    elseif ty == "SS"   ; return Set(val)
-    else                ; error("Unknown datatype value: $ty")
+    elseif ty == "L"    ; return [value_from_attributes(e) for e=val]
+    elseif ty == "NS"   ; return Set{Real}([value_from_attributes(e) for e=val])
+    elseif ty == "SS"   ; return Set{AbstractString}([value_from_attributes(e) for e=val])
+    elseif ty == "M"
+        m = Dict{AbstractString, Any}()
+        for (k, v)=val
+            m[string(k)] = value_from_attributes(v)
+        end
+        return m
+    else
+        error("Unknown datatype value in DynamoDB typed JSON value: $ty")
     end
 end
---
+++ test/dynamo_json.jl
@@ -0,0 +1,77 @@
+# write your own tests here
+
+include("../src/dynamo_json.jl")
+
+#                _       _ _
+#  ___  ___ _ __(_) __ _| (_)_______
+# / __|/ _ \ '__| |/ _` | | |_  / _ \
+# \__ \  __/ |  | | (_| | | |/ /  __/
+# |___/\___|_|  |_|\__,_|_|_/___\___|
+
+@test null_or_val(nothing) == Dict("NULL" => true)
+
+@test null_or_val(true) == Dict("BOOL" => true)
+@test null_or_val(false) == Dict("BOOL" => false)
+
+@test null_or_val("foo") == Dict("S" => "foo")
+
+@test null_or_val(21) == Dict("N" => 21)
+@test null_or_val(21.1) == Dict("N" => 21.1)
+
+@test null_or_val([1, 2, 3]) == Dict("L" => [Dict("N" => 1), Dict("N" => 2), Dict("N" => 3)])
+
+# result lists is (unsurprisingly) unordered...
+res = null_or_val(Set([1, 2, 3]))
+@test length(keys(res)) == 1
+@test length(res["NS"]) == 3
+@test contains(==, res["NS"], Dict("N" => 1))
+@test contains(==, res["NS"], Dict("N" => 2))
+@test contains(==, res["NS"], Dict("N" => 3))
+
+res = null_or_val(Set(["1", "2", "3"]))
+@test length(keys(res)) == 1
+@test length(res["SS"]) == 3
+@test contains(==, res["SS"], Dict("S" => "1"))
+@test contains(==, res["SS"], Dict("S" => "2"))
+@test contains(==, res["SS"], Dict("S" => "3"))
+
+@test null_or_val(Dict("four" => 3, 7 => 11)) == Dict("M"=>Dict("four"=>Dict("N"=>3),"7"=>Dict("N"=>11)))
+
+type Foo
+    a
+    b
+end
+
+@test null_or_val(Foo(3, "fourty")) == Dict("M"=>Dict("a"=>Dict("N"=>3),"b"=>Dict("S"=>"fourty")))
+
+
+
+#      _                     _       _ _
+#   __| | ___  ___  ___ _ __(_) __ _| (_)_______
+#  / _` |/ _ \/ __|/ _ \ '__| |/ _` | | |_  / _ \
+# | (_| |  __/\__ \  __/ |  | | (_| | | |/ /  __/
+#  \__,_|\___||___/\___|_|  |_|\__,_|_|_/___\___|
+
+function check_round_trip(val)
+    @test value_from_attributes(null_or_val(val)) == val
+end
+
+# base types
+check_round_trip(nothing)
+check_round_trip(true)
+check_round_trip(false)
+check_round_trip("foo")
+check_round_trip("false")
+check_round_trip("true")
+check_round_trip("nothing")
+check_round_trip("21")
+check_round_trip(21)
+check_round_trip(21.0)
+
+# compound types
+check_round_trip([1, 2, 3])
+check_round_trip(Set([1, 2, 3]))
+check_round_trip(Set(["1", "2", "3"]))
+
+@test value_from_attributes(null_or_val(Dict("four" => 3, 7 => 11))) == Dict("four" => 3, "7" => 11)
+@test value_from_attributes(null_or_val(Foo(3, "fourty"))) == Dict("a"=>3,"b"=>"fourty")
